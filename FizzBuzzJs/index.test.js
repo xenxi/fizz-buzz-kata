281af6382f6979587ddcf5e8d76ec5ca1e944454
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-16 11:14:10 +0200

    add test red: return 2

--- FizzBuzzJs/index.test.js
+++ FizzBuzzJs/index.test.js
@@ -5,3 +5,9 @@ describe('FizzBuzzer should', () => {
         expect(fizzBuzzer(1)).toBe('1');
     });    
 });
+
+describe('FizzBuzzer should', () => {
+    test('return 2', () => {
+        expect(fizzBuzzer(2)).toBe('2');
+    });    
+});
