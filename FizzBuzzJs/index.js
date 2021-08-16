8ae6427b2440a39cc882549aa676a03919dbaba4
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-16 13:31:20 +0200

    refact buzz guard

--- FizzBuzzJs/index.js
+++ FizzBuzzJs/index.js
@@ -1,10 +1,21 @@
 function fizzBuzzer(number) {
-  if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
-
-  if (number % 3 === 0) return "Fizz";
-  if (number % 5 === 0) return "Buzz";
+  if (isFizzBuzz(number)) return "FizzBuzz";
+  if (isFizz(number)) return "Fizz";
+  if (isBuzz(number)) return "Buzz";
 
   return `${number}`;
+}
+
+function isBuzz(number) {
+  return number % 5 === 0;
+}
+
+function isFizz(number) {
+  return number % 3 === 0;
+}
+
+function isFizzBuzz(number) {
+  return number % 3 == 0 && number % 5 == 0;
 }
 
 module.exports = fizzBuzzer;
