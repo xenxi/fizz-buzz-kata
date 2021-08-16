96d765dabeeb497ea5ce62c859d32bda4dad26f0
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-16 13:23:48 +0200

    refact

--- FizzBuzzJs/index.js
+++ FizzBuzzJs/index.js
@@ -1,6 +1,5 @@
 function fizzBuzzer(number) {
-  if (number === 15) return "FizzBuzz";
-  if (number === 30) return "FizzBuzz";
+  if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
 
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "Buzz";
