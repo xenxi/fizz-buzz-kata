9bc19efc86cef3779245ad0de5282ce70aaa7e15
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-16 13:22:38 +0200

    test green

--- FizzBuzzJs/index.js
+++ FizzBuzzJs/index.js
@@ -1,5 +1,6 @@
 function fizzBuzzer(number) {
   if (number === 15) return "FizzBuzz";
+  if (number === 30) return "FizzBuzz";
 
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "Buzz";
