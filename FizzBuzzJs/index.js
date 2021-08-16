ba81f84b2b68fcba659524973e390656442627fd
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-16 14:03:07 +0200

    refact

--- FizzBuzzJs/index.js
+++ FizzBuzzJs/index.js
@@ -1,12 +1,17 @@
 const Rules = require("./rules");
 
 function fizzBuzzer(number) {
+  const fizzBuzzRule = compositeRules();
+
+  return fizzBuzzRule.apply(number);
+}
+
+function compositeRules() {
   const defaultRule = new Rules.Default(null);
   const fizzRule = new Rules.Fizz(defaultRule);
   const buzzRule = new Rules.Buzz(fizzRule);
   const fizzBuzzRule = new Rules.FizzBuzz(buzzRule);
-
-  return fizzBuzzRule.apply(number);
+  return fizzBuzzRule;
 }
 
 module.exports = fizzBuzzer;
