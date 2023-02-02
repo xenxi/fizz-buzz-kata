4752f32115fc4bdbf421d994416090b2fac2c459
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-02 07:45:31 +0100

    ♻️ extract methods

--- FizzBuzzTs/fizz-buzzificator.ts
+++ FizzBuzzTs/fizz-buzzificator.ts
@@ -1,8 +1,11 @@
 export class FizzBuzzificator {
   public static convert(num: number): string {
-    if (num % 5 === 0 && num % 3 === 0) return "FizzBuzz";
-    if (num % 5 === 0) return "Buzz";
-    if (num % 3 === 0) return "Fizz";
+    if (this.isDivisibleBy3(num) && this.isDivisibleBy5(num)) return "FizzBuzz";
+    if (this.isDivisibleBy3(num)) return "Fizz";
+    if (this.isDivisibleBy5(num)) return "Buzz";
     return num.toString();
   }
+
+  private static isDivisibleBy3 = (num: number): boolean => num % 3 === 0;
+  private static isDivisibleBy5 = (num: number): boolean => num % 5 === 0;
 }
