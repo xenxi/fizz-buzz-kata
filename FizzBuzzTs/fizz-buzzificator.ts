45ba046ea3946c7359ea33b1ff9561097e27ac27
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-02 07:41:47 +0100

    ✅ return Buzz if divisible by 5

--- FizzBuzzTs/fizz-buzzificator.ts
+++ FizzBuzzTs/fizz-buzzificator.ts
@@ -1,5 +1,6 @@
 export class FizzBuzzificator {
   public static convert(num: number): string {
+    if (num % 5 === 0) return "Buzz";
     return "Fizz";
   }
 }
--- FizzBuzzTs/test/fizz-buzzificator.test.ts
+++ FizzBuzzTs/test/fizz-buzzificator.test.ts
@@ -8,4 +8,12 @@ describe("FizzBuzzificator should", () => {
       expect(result).toBe("Fizz");
     });
   });
+
+  test("return Buzz if divisible by 5", () => {
+    [5, 10].forEach((numberDivisible5) => {
+      const result = FizzBuzzificator.convert(numberDivisible5);
+
+      expect(result).toBe("Buzz");
+    });
+  });
 });
