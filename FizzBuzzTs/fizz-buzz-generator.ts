083b8a4d612d497361f2089a2489e3a5cbf0a592
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-02 07:56:24 +0100

    ♻️ reduce cyclomatic complexity

--- FizzBuzzTs/fizz-buzz-generator.ts
+++ FizzBuzzTs/fizz-buzz-generator.ts
@@ -2,12 +2,7 @@ import { FizzBuzzificator } from "./fizz-buzzificator";
 
 export class FizzBuzzGenerator {
   public static generate(): string[] {
-    const elements = new Array<string>();
-    for (let index = 1; index <= 100; index++) {
-      const convertNumber = FizzBuzzificator.convert(index);
-      elements.push(convertNumber);
-    }
-
-    return elements;
+    const elements = Array.from({length: 100}, (_, i) => i + 1);
+    return elements.map((num) => FizzBuzzificator.convert(num));
   }
 }
