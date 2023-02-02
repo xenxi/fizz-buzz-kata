2bee1401c1fbfe1058eca83a1f1b11fbb6b72b24
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-02 11:30:42 +0100

    ♻️ extract methods

--- FizzBuzzTs/fizz-buzz-generator.ts
+++ FizzBuzzTs/fizz-buzz-generator.ts
@@ -2,7 +2,13 @@ import { FizzBuzzificator } from "./fizz-buzzificator";
 
 export class FizzBuzzGenerator {
   public static generate(): string[] {
-    const elements = Array.from({length: 100}, (_, i) => i + 1);
-    return elements.map((num) => FizzBuzzificator.convert(num));
+    const numbers = this.generateNumbers();
+    return this.convert(numbers);
   }
+
+  private static generateNumbers = (): Array<number> =>
+    Array.from({ length: 100 }, (_, i) => i + 1);
+
+  private static convert = (numbers: Array<number>): Array<string> =>
+    numbers.map((num) => FizzBuzzificator.convert(num));
 }
